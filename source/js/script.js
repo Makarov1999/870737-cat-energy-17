99d7d0167afc6071835a392db9dc1022d60bbf57
Makarov.E <makarov.e1999@yandex.ru>
2019-08-16 11:22:00 +0400

    Написан js

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,12 @@
 var menuButton = document.querySelector(".main-nav__toggle");
 var menuList = document.querySelector(".site-list");
+var slide = document.querySelector(".example__slide--before");
+var sliderBeforeButton = document.querySelector(".slider-control__toggle--before");
+var sliderAfterButton = document.querySelector(".slider-control__toggle--after");
+
+console.log(slide);
+console.log(sliderBeforeButton);
+console.log(sliderAfterButton);
 
 menuButton.classList.remove("main-nav__toggle--close");
 menuButton.classList.add("main-nav__toggle--menu");
@@ -17,3 +24,21 @@ menuButton.addEventListener("click", function(evt) {
     menuList.classList.remove("site-list--open");
   }
 });
+
+  sliderBeforeButton.addEventListener("click", function() {
+    if (!slide.classList.contains("js-before--active") && !slide.classList.contain("js-after--active")) {
+      slide.classList.add("js-before--active");
+    } else if (slide.classList.contains("js-after--active")) {
+      slide.classList.remove("js-after--active");
+      slide.classList.add("js-before--active")
+    }
+  });
+
+  sliderAfterButton.addEventListener("click", function() {
+    if (!slide.classList.contains("js-before--active") && !slide.classList.contain("js-after--active")) {
+      slide.classList.add("js-after--active");
+    } else if (slide.classList.contains("js-before--active")) {
+      slide.classList.remove("js-before--active");
+      slide.classList.add("js-after--active")
+    }
+  });
